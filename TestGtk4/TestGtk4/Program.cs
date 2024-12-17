d00b2d37478d163a905db8a54497356681478826
Felix <credzba@gmail.com>
2024-12-17 12:31:48 -0600

    Changed to use a file for ui definition, but left resource loads in project file for future reference

--- TestGtk4/TestGtk4/Program.cs
+++ TestGtk4/TestGtk4/Program.cs
@@ -3,40 +3,101 @@ using Gtk.Internal;
 using GLib;
 using System;
 using System.Text;
+using System.Net.Security;
+using System.Reflection;
 
-var application = Gtk.Application.New("org.GirCore.GTK4Counter", Gio.ApplicationFlags.FlagsNone);
-application.OnActivate += (sender, args) =>
+public class TestGtk4
 {
-    var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-    var resourceNames = assembly.GetManifestResourceNames();
-    foreach (var name in resourceNames)
+    public static void Main(string[] args)
     {
-        Console.WriteLine($"Available Resource: {name}");
-        using Stream? stream = assembly.GetManifestResourceStream(name);
-        using var ms = new MemoryStream();
-        stream.CopyTo(ms);
+        var application = Gtk.Application.New("org.GirCore.GTK4Counter", Gio.ApplicationFlags.FlagsNone);
+        application.OnActivate += OnActivate;
+        //application.OnActivate += (sender, e) => OnActivate(application, e);
 
-        var buffer = ms.ToArray();
 
-        string test = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        var assembly = typeof(Gtk.Application).Assembly;
+        Console.WriteLine($"Assembly Name: {assembly.FullName}");
+        Console.WriteLine($"Assembly Location: {assembly.Location}");
+        Console.WriteLine($"Assembly CodeBase: {assembly.CodeBase}");
+        Console.WriteLine($"GTK Assembly Version: {typeof(Gtk.Application).Assembly.GetName().Version}");
+
+        application.Run(args.Length, args); // WithSynchronizationContext();
     }
 
-    // Load the UI from a Builder file    
-    //var builder = new Gtk.Builder("TestGtk4.window.ui");
-    var builder = new Gtk.Builder("TestGtk4.ServerSelection.ui");
-    //builder.AddFromString("window.ui", 9);
+    private static void OnActivate(Gio.Application application, EventArgs args)
+    {
+        var gtkApplication = application as Gtk.Application;
+        if (gtkApplication == null)
+        {
+            throw new InvalidCastException("The application is not a Gtk.Application.");
+        }
 
-    // Get the window object from the UI file
-    var window = (Gtk.Window)builder.GetObject("main_window");
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceNames = assembly.GetManifestResourceNames();
+        foreach (var name in resourceNames)
+        {
+            Console.WriteLine($"Available Resource: {name}");
+            using Stream? stream = assembly.GetManifestResourceStream(name);
+            using var ms = new MemoryStream();
+            stream.CopyTo(ms);
 
-    // Show the window and all its contents
-    window.SetApplication(application);
-    window.Show();
-};
-var assembly = typeof(Gtk.Application).Assembly;
-Console.WriteLine($"Assembly Name: {assembly.FullName}");
-Console.WriteLine($"Assembly Location: {assembly.Location}");
-Console.WriteLine($"Assembly CodeBase: {assembly.CodeBase}");
-Console.WriteLine($"GTK Assembly Version: {typeof(Gtk.Application).Assembly.GetName().Version}");
+            var buffer = ms.ToArray();
 
-return application.Run(1, ["none"]); // WithSynchronizationContext();
+            string test = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        }
+
+        string builderSourceFile = "ServerSelection.ui";
+        if (FileFinder.FindFileUpwards(builderSourceFile, out string completePath))
+        {
+            // Load the UI from a Builder file    
+            var builder = Gtk.Builder.NewFromFile(completePath);
+
+            // Get the window object from the UI file
+            var window = (Gtk.Window)builder.GetObject("main_window");
+
+            // Show the window and all its contents
+            window.SetApplication(gtkApplication);
+            window.Show();
+        }
+        else
+        {
+            Console.WriteLine($"File: {builderSourceFile} not found.");
+        }
+    }
+}
+
+
+public class FileFinder
+{
+    public static bool FindFileUpwards(string filename, out string completePath)
+    {
+        // Start from the current directory
+        DirectoryInfo currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+        while (true)
+        {
+            // Combine the current path and the filename
+            FileInfo candidate = new FileInfo(Path.Combine(currentDirectory.FullName, filename));
+
+            // Check if the file exists in the current directory
+            if (candidate.Exists)
+            {
+                // Return the complete path as a string
+                completePath = candidate.FullName;
+                return true;
+            }
+
+            // If we are at the root directory, stop searching
+            if (currentDirectory.Parent == null)
+            {
+                break;
+            }
+
+            // Move up one directory level
+            currentDirectory = currentDirectory.Parent;
+        }
+
+        completePath = null;
+        return false; // File not found
+    }
+}
